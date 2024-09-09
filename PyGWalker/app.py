d9c3ac7d95b07c6f86a7408829f6b7065e8a74df
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-09-09 23:53:24 +0300

    minor fixes
    removed submit form
    added sample data

--- PyGWalker/app.py
+++ PyGWalker/app.py
@@ -13,15 +13,14 @@ def load_data(data):
 def main():
     st.title("Streamlit PyGWalker App")
     st.subheader("Home")
-    with st.form("upload_form"):
-        data_file = st.file_uploader("Upload a CSV File", type=["csv","txt"])
-        submitted = st.form_submit_button("Submit")
+    data_file = st.file_uploader("Upload a CSV File", type=["csv","txt"])
+    if data_file is None:
+        data_file = "sample.csv"
 
-    if submitted:
-        df = load_data(data_file)
-        st.dataframe(df)
-        pyg_app = StreamlitRenderer(df)
-        pyg_app.explorer()   
+    df = load_data(data_file)
+    st.dataframe(df)
+    pyg_app = StreamlitRenderer(df)
+    pyg_app.explorer()   
     
 if __name__ == "__main__":
     main()
